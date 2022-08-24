1dfa3bf65e959193072884bb758a8bf3e9ecc3ed
anticher <anticherq@gmail.com>
2022-08-24 11:26:53 +0300

    feat: add config-module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PublicModule } from './public/public.module';
 import { PrivateModule } from './private/private.module';
 
+const envType = process.env.NODE_ENV;
+
 @Module({
-  imports: [PublicModule, PrivateModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: !envType ? '.env' : `.env.${envType}`,
+    }),
+    PublicModule,
+    PrivateModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
